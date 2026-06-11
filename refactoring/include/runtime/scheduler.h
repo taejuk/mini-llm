b42786da724146b5a0b9cf02d3dd0e347a84c8c1
taejuk <taeju456@gmail.com>
2026-06-11 11:16:49 +0900

    scheduler 모듈화

--- refactoring/include/runtime/scheduler.h
+++ refactoring/include/runtime/scheduler.h
@@ -3,10 +3,12 @@
 #include <uv.h>
 #include <atomic>
 #include <iostream>
+#include "model/gpt2_model.cuh"
 #include "runtime/request.h"
 #include "runtime/response.h"
 #include "runtime/mutex_queue.h"
-
+#include "runtime/block_manager.h"
+#include "runtime/pool.cuh"
 namespace mini_llm::runtime {
 class Scheduler {
 // Server 관련 변수들
@@ -14,22 +16,32 @@ private:
     MutexQueue<std::unique_ptr<Request>>& request_queue_;
     MutexQueue<Response>& response_queue_;
     uv_async_t* response_async_ = nullptr;
+    
 // Scheduler 변수들
 private:
-    MutexQueue<std::unique_ptr<Request>> waiting_prefill_queue_;
-    MutexQueue<std::unique_ptr<Request>> prefill_queue_;
-    MutexQueue<std::unique_ptr<Request>> decode_queue_;
-    MutexQueue<std::unique_ptr<Request>> finish_queue_;
-    MutexQueue<std::unique_ptr<Request>> deferred_queue_;
-    MutexQueue<std::unique_ptr<Request>> cancel_queue_;
+    std::deque<std::unique_ptr<Request>> waiting_prefill_queue_;
+    // prefill이 가능한 queue
+    std::deque<std::unique_ptr<Request>> prefill_queue_;
+    std::deque<std::unique_ptr<Request>> decode_queue_;
+    std::deque<std::unique_ptr<Request>> finish_queue_;
+    
+    std::deque<std::unique_ptr<Request>> deferred_prefill_queue_;
+    std::deque<std::unique_ptr<Request>> deferred_decode_queue_;
+    std::deque<std::unique_ptr<Request>> cancel_queue_;
     std::atomic<bool> running_{false};
     std::thread worker_;
+    BlockManager block_manager_;
+    GPT2Model model_;
 public:
     Scheduler(MutexQueue<std::unique_ptr<Request>>& request_queue, MutexQueue<Response>& response_queue, uv_async_t* response_async)
         : request_queue_(request_queue),
           response_queue_(response_queue),
           response_async_(response_async)
-        {}
+        {
+            Pool pool = Pool::getInstance();
+            block_manager_ = BlockManager::getInstance(pool);
+            model_ = GPT2Model::get();
+        }
     ~Scheduler();
 
     Scheduler(const Scheduler&) = delete;
@@ -40,5 +52,16 @@ public:
 
 private:
     void worker_loop();
+
+    bool try_admit_prefill(std::unique_ptr<Request>& req);
+    bool try_admit_decode(std::unique_ptr<Request>& req);
+
+    void drain_new_requests();
+    void retry_deferred_prefill();
+    void retry_deferred_decode();
+    void run_decode_batch();
+    void run_prefill_batch();
+    bool no_internal_work();
+    void wait_and_enqueue_one_request();
 };
 }
--- refactoring/src/runtime/scheduler.cpp
+++ refactoring/src/runtime/scheduler.cpp
@@ -1,5 +1,5 @@
 #include "runtime/scheduler.h"
-
+#include "runtime/block.h"
 
 namespace mini_llm::runtime {
 
@@ -22,49 +22,103 @@ void Scheduler::stop() {
     //if(worker_.joinable()) worker_.join();
     
 }
+
+bool Scheduler::try_admit_prefill(std::unique_ptr<Request>& req) {
+    int need_blocks_per_layer = (req->tokens.size() + C::DEFAULT_KV_BLOCK_SIZE - 1) / C::DEFAULT_KV_BLOCK_SIZE;
+    int total_need = need_blocks_per_layer * C::GPT2_N_LAYERS;
+
+    if (!block_manager_.can_allocate(total_need)) {
+        return false;
+    }
+
+    for (int layer = 0; layer < C::GPT2_N_LAYERS; layer++) {
+        for (int i = 0; i < need_blocks_per_layer; i++) {
+            int block_id = block_manager_.allocate_one();
+            req->layer_kv[layer].append_block(block_id);
+        }
+    }
+
+    return true;
+}
+
+bool Scheduler::try_admit_decode(std::unique_ptr<Request>& req) {
+    // 추가한다는 것은 
+    int need_blocks_per_layer = (req->tokens.size() %  C::DEFAULT_KV_BLOCK_SIZE) == 0 ? 1 : 0;
+    int total_need = need_blocks_per_layer * C::GPT2_N_LAYERS;
+    if(!block_manager_.can_allocate(total_need)) return false;
+
+    for (int layer = 0; layer < C::GPT2_N_LAYERS; layer++) {
+        for (int i = 0; i < need_blocks_per_layer; i++) {
+            int block_id = block_manager_.allocate_one();
+            req->layer_kv[layer].append_block(block_id);
+        }
+    }
+
+    return true;
+}
+
+void Scheduler::drain_new_requests() {
+    std::unique_ptr<Request> req;
+    int nums = 0;
+    while(nums < C::MAX_BATCH_NUM && request_queue_.try_pop(req)) {
+        waiting_prefill_queue_.push_back(std::move(req));
+        nums++;
+    }
+    return;
+}
+
+void Scheduler::retry_deferred_prefill() {
+    // prefill이 가능한 queue에 넣는다.
+    // 이 때 우선순위로 넣어야 하니깐 queue의 앞에 넣는다.
+    int n = deferred_prefill_queue_.size();
+    for(int i = 0; i < n; i++) {
+        auto req = std::move(deferred_prefill_queue_.front());
+        deferred_prefill_queue_.pop_front();
+
+        if (try_admit_prefill(req)) prefill_queue_.push_front(std::move(req));
+        else {
+            deferred_prefill_queue_.push_back(std::move(req));
+        } 
+    }
+}
+
+void Scheduler::retry_deferred_decode() {
+    
+}
+
+void Scheduler::run_decode_batch() {
+    
+}
+
+void Scheduler::run_prefill_batch() {
+
+}
+
+bool Scheduler::no_internal_work() {
+    
+    return false;
+}
+
+void Scheduler::wait_and_enqueue_one_request() {
+    
+}
+
 // 여기에 로직이 담겨져 있어야 한다.
 void Scheduler::worker_loop() {
     // 여기서 model을 가지고 와야 한다.
     while (running_.load()) {
+        drain_new_requests();
 
-        if(decode_queue_.empty() && prefill_queue_.empty()) {
-            std::optional<std::unique_ptr<Request>> req_opt = request_queue_.wait_pop();
-            if(!req_opt.has_value()) break;
-            std::vector<std::unique_ptr<Request>> reqs;
-            reqs.push_back(std::move(req_opt.value()));
-            request_queue_.drain(reqs);
-            std::cout << "requests: " << reqs.size() << std::endl;
-            for(int i = 0; i < reqs.size(); i++) prefill_queue_.push(std::move(reqs[i]));
+        retry_deferred_decode();
 
-        }
-        int i = 0;
-        int max_i = decode_queue_.size();
-        while(i < max_i) {
-            // 하나씩 꺼내서 decode를 실행할 수 있는지 평가해야 한다.
+        retry_deferred_prefill();
 
-            // 
-        }
+        run_decode_batch();
+
+        run_prefill_batch();
+
+        if(no_internal_work()) wait_and_enqueue_one_request();    
         
-        // prefill에 대해서 처리한다.
-        //while(!prefill_queue_.empty()) {}
-        for(int i = 0; i < prefill_queue_.size(); i++) {
-            // BATCH NUM까지 꺼낸다.
-
-            // batch_prefill을 실행한다.
-
-            // response를 보고 done인지 아닌지 판단한 후에,
-            // done이면 free하고 new_decode_queue 안 넣는다.
-
-            // done이 아니면 다시 new_decode_queue 넣는다.
-        }
-        // 들어온 response들을 drain한다.
-        
-        std::optional<std::unique_ptr<Request>> req_opt = request_queue_.wait_pop();
-        if(!req_opt.has_value()) break;
-        std::vector<std::unique_ptr<Request>> reqs;
-        reqs.push_back(std::move(req_opt.value()));
-        request_queue_.drain(reqs);
-        for(int i = 0; i < reqs.size(); i++) prefill_queue_.push(std::move(reqs[i]));
     }
     // 여기서 호출하면 된다.
 }
