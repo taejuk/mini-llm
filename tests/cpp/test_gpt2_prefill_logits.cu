85e931f7ef8403171a76b1677e334298dcd499b6
taejuk <32951995+taejuk@users.noreply.github.com>
2026-06-14 18:41:04 +0900

    Use model test helper implementation from model library

--- tests/cpp/test_gpt2_prefill_logits.cu
+++ tests/cpp/test_gpt2_prefill_logits.cu
@@ -18,22 +18,6 @@ namespace C = mini_llm::constants;
 namespace Rt = mini_llm::runtime;
 namespace Model = mini_llm::model;
 
-namespace mini_llm::model {
-
-std::vector<float> GPT2Model::prefill_logits_for_test(
-    std::vector<std::unique_ptr<Rt::Request>>& reqs
-) {
-    prefill(reqs);
-
-    size_t n =
-        static_cast<size_t>(reqs.size()) *
-        static_cast<size_t>(mini_llm::constants::GPT2_VOCAB_SIZE);
-
-    return std::vector<float>(h_logits, h_logits + n);
-}
-
-} // namespace mini_llm::model
-
 namespace {
 
 template <typename T>
