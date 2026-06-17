753756dad97971c102bb2951c5b471dee59d8155
taejuk <taeju456@gmail.com>
2026-06-17 09:14:11 +0900

    kv allocator 에러 수정

--- include/runtime/real_kv_allocator.h
+++ include/runtime/real_kv_allocator.h
@@ -7,6 +7,7 @@
 #include "runtime/cpu_pool.cuh"
 #include "constants.h"
 
+#include <cstdint>
 #include <unordered_map>
 
 namespace mini_llm::runtime {
@@ -17,11 +18,12 @@ private:
     CpuPool& cpu_pool_;
     BlockManager& block_manager_;
     std::unordered_map<uint64_t, SwappedRequest> swapped_requests_;
-    // swap in 할 때 사용하는 buffer이다.
+
+    // swap_in staging buffer:
+    // CPU contiguous KV blocks -> GPU staging -> scatter to GPU KV blocks
     float* d_swap_in_staging_ = nullptr;
     int* d_swap_in_dst_block_ids_ = nullptr;
-
-    int swap_in_staging_capacity_blocks_ = 0;
+    int swap_in_capacity_blocks_ = 0;
 
     bool init_swap_in_buffers();
     void release_swap_in_buffers();
@@ -31,11 +33,10 @@ public:
         : pool_(Pool::getInstance(mini_llm::constants::DEFAULT_TOTAL_KV_BLOCKS)),
           cpu_pool_(CpuPool::getInstance(mini_llm::constants::DEFAULT_TOTAL_CPU_BLOCKS)),
           block_manager_(BlockManager::getInstance(pool_, cpu_pool_)) {
-            size_t size = mini_llm::constants::MAX_SEQ * 2 * mini_llm::constants::GPT2_D_MODEL * sizeof(float);
-            cudaMalloc(&buffer_, size);
-          }
-        
-    ~RealKvAllocator();
+        init_swap_in_buffers();
+    }
+
+    ~RealKvAllocator() override;
 
     bool allocate_prefill(Request& req) override;
     bool allocate_decode(Request& req) override;
