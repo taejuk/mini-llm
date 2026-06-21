d97dd6241264fdb71c068d20b3ddb41814c6118f
taejuk <taeju456@gmail.com>
2026-06-21 14:01:48 +0900

    decode timer 추가

--- src/runtime/scheduler.cpp
+++ src/runtime/scheduler.cpp
@@ -326,7 +326,8 @@ void Scheduler::worker_loop() {
         if (!admission_paused_) {
             drain_new_requests();
         }
-
+        // swap_in을 비동기로 처리하자.
+        // 
         is_progress |= retry_swap_in_requests();
 
         retry_deferred_decode();
